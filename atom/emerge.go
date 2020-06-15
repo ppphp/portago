916e446b9c93f6d6b5bd6f99881eff20e2cc73f3
fengche <2395434298@qq.com>
2020-06-15 10:50:22 +0800

    change

--- atom/emerge.go
+++ atom/emerge.go
@@ -2561,6 +2561,111 @@ func (b *BlockerDB)discardBlocker( pkg) {
 	}
 }
 
+type CompositeTask struct {
+	*AsynchronousTask
+
+	// slot
+	_current_task *SpawnProcess
+
+	_TASK_QUEUED int
+}
+
+func (c*CompositeTask)_cancel() {
+	if c._current_task != nil {
+		if c._current_task is
+		c._TASK_QUEUED{
+			c.returncode = new(int)
+			*c.returncode = 1
+			c._current_task = nil
+			c._async_wait()
+		} else {
+			c._current_task.cancel()
+		}
+	} else if c.returncode == nil {
+		c._was_cancelled()
+		c._async_wait()
+	}
+}
+
+func(c*CompositeTask) _poll() {
+	prev = nil
+	for true {
+		task = c._current_task
+		if task == nil ||
+			task
+			is
+		c._TASK_QUEUED
+||
+task
+is
+prev{
+break
+}
+task.poll()
+prev = task
+}
+return c.returncode
+}
+
+func(c*CompositeTask) _assert_current(task *SpawnProcess) {
+	if task != c._current_task {
+		raise
+		AssertionError("Unrecognized task: %s" % (task, ))
+	}
+}
+
+func(c*CompositeTask) _default_exit( task *SpawnProcess) int {
+	c._assert_current(task)
+	if task.returncode != 0 {
+		c.returncode = task.returncode
+		c.cancelled = task.cancelled
+		c._current_task = nil
+		return *task.returncode
+	}
+}
+func(c*CompositeTask) _final_exit( task) {
+	c._default_exit(task)
+	c._current_task = nil
+	c.returncode = task.returncode
+	return c.returncode
+}
+
+func(c*CompositeTask) _default_final_exit( task) {
+	c._final_exit(task)
+	return c.wait()
+}
+
+func(c*CompositeTask) _start_task(task *SpawnProcess, exit_handler) {
+	//try{
+	//task.scheduler = c.scheduler
+	//except AttributeError{
+	//pass
+	task.addExitListener(exit_handler)
+	c._current_task = task
+	task.start()
+}
+
+func(c*CompositeTask) _task_queued(task *EbuildPhase) {
+	task.addStartListener(c._task_queued_start_handler)
+	c._current_task = c._TASK_QUEUED
+}
+
+func(c*CompositeTask) _task_queued_start_handler( task) {
+	c._current_task = task
+}
+
+func(c*CompositeTask) _task_queued_wait() {
+	return c._current_task != c._TASK_QUEUED ||
+		c.cancelled || c.returncode != nil
+}
+
+func NewCompositeTask()*CompositeTask {
+	c := &CompositeTask{}
+	c._TASK_QUEUED = -1
+	c.AsynchronousTask = NewAsynchronousTask()
+	return c
+}
+
 type SubProcess struct {
 	*AbstractPollTask
 	pid, _waitpid_id int
@@ -3239,111 +3344,6 @@ func (e *EbuildSpawnProcess)_spawn( args, **kwargs) {
 	}
 
 	return e.spawn_func(args, env = env, **kwargs)
-}
-
-type CompositeTask struct {
-	*AsynchronousTask
-
-	// slot
-	_current_task *SpawnProcess
-
-	_TASK_QUEUED int
-}
-
-func NewCompositeTask()*CompositeTask {
-	c := &CompositeTask{}
-	c._TASK_QUEUED = -1
-	c.AsynchronousTask = NewAsynchronousTask()
-	return c
-}
-
-func (c*CompositeTask)_cancel() {
-	if c._current_task != nil {
-		if c._current_task is
-		c._TASK_QUEUED{
-			c.returncode = new(int)
-			*c.returncode = 1
-			c._current_task = nil
-			c._async_wait()
-		} else {
-			c._current_task.cancel()
-		}
-	} else if c.returncode == nil {
-		c._was_cancelled()
-		c._async_wait()
-	}
-}
-
-func(c*CompositeTask) _poll() {
-	prev = nil
-	for true {
-		task = c._current_task
-		if task == nil ||
-		task
-		is
-		c._TASK_QUEUED
-		||
-		task
-		is
-		prev{
-			break
-		}
-		task.poll()
-		prev = task
-	}
-	return c.returncode
-}
-
-func(c*CompositeTask) _assert_current(task *SpawnProcess) {
-	if task != c._current_task {
-		raise
-		AssertionError("Unrecognized task: %s" % (task, ))
-	}
-}
-
-func(c*CompositeTask) _default_exit( task *SpawnProcess) int {
-	c._assert_current(task)
-	if task.returncode != 0 {
-		c.returncode = task.returncode
-		c.cancelled = task.cancelled
-		c._current_task = nil
-		return *task.returncode
-	}
-}
-func(c*CompositeTask) _final_exit( task) {
-	c._default_exit(task)
-	c._current_task = nil
-	c.returncode = task.returncode
-	return c.returncode
-}
-
-func(c*CompositeTask) _default_final_exit( task) {
-	c._final_exit(task)
-	return c.wait()
-}
-
-func(c*CompositeTask) _start_task(task *SpawnProcess, exit_handler) {
-	//try{
-	//task.scheduler = c.scheduler
-	//except AttributeError{
-	//pass
-	task.addExitListener(exit_handler)
-	c._current_task = task
-	task.start()
-}
-
-func(c*CompositeTask) _task_queued(task *EbuildPhase) {
-	task.addStartListener(c._task_queued_start_handler)
-	c._current_task = c._TASK_QUEUED
-}
-
-func(c*CompositeTask) _task_queued_start_handler( task) {
-	c._current_task = task
-}
-
-func(c*CompositeTask) _task_queued_wait() {
-	return c._current_task != c._TASK_QUEUED ||
-		c.cancelled || c.returncode != nil
 }
 
 
